9a99580003a3a654d0834918ed22d4b0681d1c06
ben256dev <benjamin@ben256.com>
2025-07-18 08:59:58 -0600

    new user otp noreply

--- install-shl.sh
+++ install-shl.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cp shl /bin/shl
+cp shl /bin/
 sudo chmod 755 /bin/shl
 chown sht:sht /bin/shl
+
+cp shl-lpasswd /bin/
+sudo chmod 700 /bin/shl-lpasswd
+chown root:root /bin/shl-lpasswd
+
+cp shl-upasswd /bin/
+sudo chmod 700 /bin/shl-upasswd
+chown root:root /bin/shl-upasswd
